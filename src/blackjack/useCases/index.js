58ff934ae83ee5ce00db48a2da131bdff89f62f3
iamjohangalvan <iamjohangalvan@gmail.com>
2025-07-01 14:54:06 -0400

    first commit

---
+++ src/blackjack/useCases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from "./crear-deck";
+export { pedirCarta } from "./pedir-carta";
+export { crearCarta } from "./crear-carta";
+export { turnoComputadora } from "./turno-pc";
+export { valorCarta } from "./valor-carta";
---
+++ src/blackjack/useCases/turno-pc.js
@@ -0,0 +1,45 @@
+import { pedirCarta, valorCarta, crearCarta } from "./";
+
+/**
+ * turno de la computadora
+ * @param {Number} puntosMinimos puntos que la pc necesita para ganar
+ * @param {HTMLElement} puntosHTML elemento HTML para mostrar puntos
+ * @param {HTMLElement} divCartasComputadora elemento HTML para mostrar cartas
+ * @param {Array<String>} deck
+ */
+export const turnoComputadora = (
+  puntosMinimos,
+  puntosHTML,
+  divCartasComputadora,
+  deck = []
+) => {
+  if (!puntosMinimos) throw new Error("Puntos minimos son requeridos");
+  if (!puntosHTML) throw new Error("puntosHTML es requerido");
+  let puntosComputadora = 0;
+  do {
+    const carta = pedirCarta(deck);
+
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHTML.innerText = puntosComputadora;
+
+    const imgCarta = crearCarta(carta);
+
+    divCartasComputadora.append(imgCarta);
+
+    if (puntosMinimos > 21) {
+      break;
+    }
+  } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+
+  setTimeout(() => {
+    if (puntosComputadora === puntosMinimos) {
+      alert("Nadie gana :(");
+    } else if (puntosMinimos > 21) {
+      alert("Computadora gana");
+    } else if (puntosComputadora > 21) {
+      alert("Jugador Gana");
+    } else {
+      alert("Computadora Gana");
+    }
+  }, 100);
+};
